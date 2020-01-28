415a7bcd734987278828474f5bce0b0517b79f49
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-01-29 00:20:05 +0200

    Init project

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+	[SerializeField] int max = 1000;
+	[SerializeField] int min = 1;
+	[SerializeField] TextMeshProUGUI guessText;
+	int guess;
+
+	void Start()
+	{
+		UpdateGuess();
+	}
+
+	public void OnPressHigher() 
+	{
+		min = guess + 1;
+		UpdateGuess();
+	}
+
+	public void OnPressLower() 
+	{
+		max = guess - 1;
+		UpdateGuess();
+	}
+
+	void UpdateGuess() 
+	{
+		guess = Random.Range(min, max + 1);
+		guessText.text = guess.ToString();
+	}
+}
